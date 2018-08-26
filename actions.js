e95054253db279c0c8cb11677f179a79a149dc33
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-26 15:02:24 +0200

    init project

---
+++ actions.js
@@ -0,0 +1,13 @@
+
+const ADD_COMMENT = 'ADD_COMMENT';
+
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    id: uuid.v4(),
+    text: text
+  }
+}
+
+export ADD_COMMENT;
+export addComment;
